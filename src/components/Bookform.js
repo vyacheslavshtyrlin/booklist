ceae96bad6cb3de73f97a845be5ac528ae1b1302
VyacheslavShtyrlin <vyacheslavshtyrlin@yandex.ru>
2022-06-09 19:06:02 +0500

    мини-валидация

--- src/components/Bookform.js
+++ src/components/Bookform.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
-export default function BookForm({ onSubmit}) {
+export default function BookForm({ onSubmit }) {
   const [name, setName] = useState("");
   const [author, setAuthor] = useState("");
+  const [warning, setWarning] = useState(false);
   const handleInputName = (e) => {
     setName(e.target.value);
   };
@@ -10,16 +11,22 @@ export default function BookForm({ onSubmit}) {
   const handleInputAuthor = (e) => {
     setAuthor(e.target.value);
   };
+  console.log(warning);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSubmit({
-      id: Math.floor(Math.random() * 10000),
-      name: name,
-      author: author,
-    });
-    setName("");
-    setAuthor("");
+    if (name && author) {
+      onSubmit({
+        id: Math.floor(Math.random() * 10000),
+        name: name,
+        author: author,
+      });
+      setName("");
+      setAuthor("");
+      setWarning(false);
+    } else {
+      setWarning(true);
+    }
   };
 
   return (
@@ -37,6 +44,11 @@ export default function BookForm({ onSubmit}) {
         onChange={handleInputAuthor}
         value={author}
       ></input>
+      {warning ? (
+        <span className="form__warning">Все поля должны быть заполнены</span>
+      ) : (
+        <></>
+      )}
       <button className="form__button-submit" onClick={handleSubmit}>
         Добавить
       </button>
